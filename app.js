d548add4fbbb227f2d05e7c04f3f0d7a5f747bab
pribula329 <pribula329@gmail.com>
2023-07-28 08:37:37 +0200

    extract file

---
+++ app.js
@@ -0,0 +1,61 @@
+Vue.createApp({
+
+    data() {
+        return {
+            shopName:[
+                "Lidl",
+                "Tesco",
+                "Kaufland"
+            ]
+        };
+    }
+})
+    .component('shop',{
+        template: '#shop-template',
+        props:{
+            name: String
+        },
+        data(){
+            return{
+                header: "Shopping List",
+                show: false,
+                textShow: "Show items",
+                newItem: "",
+                items: [
+                    {id:0, label: "10 t-shirt"},
+                    {id:1, label:"5 trousers"},
+                    {id:2, label:"2 cap"},
+                    {id:6, label:"2 cap"},
+                ]
+            }
+        },
+        methods:{
+            saveItem(){
+                this.items.push({
+                    id: this.items.length+1,
+                    label: this.newItem
+                })
+                this.newItem=""
+                if (this.show===false){
+                    this.doShow()
+                }
+
+            },
+            doShow(){
+                this.show = !this.show
+                if (this.textShow==="Show items"){
+                    this.textShow="Hide items"
+                }
+                else {
+                    this.textShow="Show items"
+                }
+            }
+        },
+        computed:{
+            reverseItem(){
+                return [...this.items].reverse()
+            }
+        }
+
+    })
+    .mount("#shopping-list")
